b3beec41a2e89b3bc350fd8042379ef9a9f0c989
Manuito83 <manuortega@gmail.com>
2021-03-05 22:12:28 +0100

    Added userscript usage example

--- lib/widgets/webviews/webview_full.dart
+++ lib/widgets/webviews/webview_full.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:collection';
 import 'package:animations/animations.dart';
 import 'package:bot_toast/bot_toast.dart';
 import 'package:bubble_showcase/bubble_showcase.dart';
@@ -138,12 +139,16 @@ class _WebViewFullState extends State<WebViewFull> {
   SettingsProvider _settingsProvider;
   ThemeProvider _themeProvider;
 
+  String path;
+
   @override
   void initState() {
     super.initState();
     _loadChatPreferences();
     _settingsProvider = Provider.of<SettingsProvider>(context, listen: false);
-    _initialUrl = URLRequest(url: Uri.parse(widget.customUrl));
+    _initialUrl = URLRequest(
+        url: Uri.parse(widget.customUrl),
+        headers: <String, String>{'test_header': 'flutter_test_header'});
     _pageTitle = widget.customTitle;
   }
 
@@ -452,6 +457,7 @@ class _WebViewFullState extends State<WebViewFull> {
         Expanded(
           child: InAppWebView(
             initialUrlRequest: _initialUrl,
+            initialUserScripts: UnmodifiableListView<UserScript>([lala]),
             initialOptions: InAppWebViewGroupOptions(
               crossPlatform: InAppWebViewOptions(
                   // This is deactivated as it interferes with hospital timer,
@@ -459,37 +465,33 @@ class _WebViewFullState extends State<WebViewFull> {
                   //useShouldInterceptAjaxRequest: true,
                   ),
               android: AndroidInAppWebViewOptions(
-                //builtInZoomControls: false,
                 useHybridComposition: true,
-                //useWideViewPort: false,
-                //loadWithOverviewMode: true,
-                //displayZoomControls: true,
               ),
             ),
             /*
-                  shouldInterceptAjaxRequest:
-                      (InAppWebViewController c, AjaxRequest x) async {
-                    // This will intercept ajax calls performed when the bazaar reached 100 items
-                    // and needs to be reloaded, so that we can remove and add again the fill buttons
-                    if (x == null) return x;
-                    if (x.data == null) return x;
-                    if (x.url == null) return x;
+            shouldInterceptAjaxRequest:
+                (InAppWebViewController c, AjaxRequest x) async {
+              // This will intercept ajax calls performed when the bazaar reached 100 items
+              // and needs to be reloaded, so that we can remove and add again the fill buttons
+              if (x == null) return x;
+              if (x.data == null) return x;
+              if (x.url == null) return x;
 
-                    if (x.data.contains("step=getList&type=All&start=") &&
-                        x.url.contains('inventory.php') &&
-                        _bazaarActive &&
-                        _bazaarFillActive) {
-                      webView.evaluateJavascript(
-                          source: removeBazaarFillButtonsJS());
-                      Future.delayed(const Duration(seconds: 2))
-                          .then((value) {
-                        webView.evaluateJavascript(
-                            source: addBazaarFillButtonsJS());
-                      });
-                    }
-                    return x;
-                  },
-                  */
+              if (x.data.contains("step=getList&type=All&start=") &&
+                  x.url.contains('inventory.php') &&
+                  _bazaarActive &&
+                  _bazaarFillActive) {
+                webView.evaluateJavascript(
+                    source: removeBazaarFillButtonsJS());
+                Future.delayed(const Duration(seconds: 2))
+                    .then((value) {
+                  webView.evaluateJavascript(
+                      source: addBazaarFillButtonsJS());
+                });
+              }
+              return x;
+            },
+            */
             onWebViewCreated: (InAppWebViewController c) {
               webView = c;
             },
@@ -1826,3 +1828,117 @@ class _WebViewFullState extends State<WebViewFull> {
     return false;
   }
 }
+
+UserScript lala = UserScript(
+  injectionTime: UserScriptInjectionTime.AT_DOCUMENT_START,
+  source: r"""
+// ==UserScript==
+// @name         Torn Custom Race Presets
+// @namespace    https://greasyfork.org/en/scripts/393632-torn-custom-race-presets
+// @version      0.2.1
+// @description  Make it easier and faster to make custom races - Extended from Xiphias's
+// @author       Cryosis7 [926640]
+// @match        *www.torn.com/loader.php?sid=racing*
+// @grant        none
+// ==/UserScript==
+ 
+ 
+/**
+ * Modify the presets as you see fit, you can add and remove presets,
+ * or remove individual fields within the preset to only use the fields you care about.
+ * 
+ * TEMPLATE
+ * {
+        name: "Appears as the button name and the public name of the race",
+        maxDrivers: 6,
+        trackName: "Industrial",
+        numberOfLaps: 1,
+        upgradesAllowed: true,
+        betAmount: 0,
+        waitTime: 1,
+        password: "",
+    },
+ * 
+ */
+var presets = [{
+        name: "Quick Industrial",
+        maxDrivers: 2,
+        trackName: "Industrial",
+        numberOfLaps: 1,
+        upgradesAllowed: true,
+        betAmount: 0,
+        waitTime: 1,
+        password: "",
+    },
+    {
+        name: "1hr Start - Docks",
+        maxDrivers: 100,
+        trackName: "Docks",
+        numberOfLaps: 100,
+        waitTime: 60,
+        password: "",
+    },
+];
+ 
+(function() {
+    'use strict';
+    scrubPresets();
+    $('body').ajaxComplete(function(e, xhr, settings) {
+        var createCustomRaceSection = "section=createCustomRace";
+        var url = settings.url;
+        if (url.indexOf(createCustomRaceSection) >= 0) {
+            scrubPresets();
+            drawPresetBar();
+        }
+    });
+})();
+ 
+function fillPreset(index) {
+    let race = presets[index];
+ 
+    if ("name" in race) $('.race-wrap div.input-wrap input').attr('value', race.name);
+    if ("maxDrivers" in race) $('.drivers-max-wrap div.input-wrap input').attr('value', race.maxDrivers);
+    if ("numberOfLaps" in race) $('.laps-wrap > .input-wrap > input').attr('value', race.numberOfLaps);
+    if ("betAmount" in race) $('.bet-wrap > .input-wrap > input').attr('value', race.betAmount);
+    if ("waitTime" in race) $('.time-wrap > .input-wrap > input').attr('value', race.waitTime);
+    if ("password" in race) $('.password-wrap > .input-wrap > input').attr('value', race.password);
+ 
+    if ("trackName" in race) {
+        $('#select-racing-track').selectmenu();
+        $('#select-racing-track-menu > li:contains(' + race.trackName + ')').mouseup();
+    }
+    if ("upgradesAllowed" in race) {
+        $('#select-allow-upgrades').selectmenu();
+        $('#select-allow-upgrades-menu > li:contains(' + race.upgradesAllowedString + ')').mouseup();
+    }
+}
+ 
+function scrubPresets() {
+    presets.forEach(x => {
+        if ("name" in x && x.name.length > 25) x.name = x.name.substring(0, 26);
+        if ("maxDrivers" in x) x.maxDrivers = (x.maxDrivers > 100) ? 100 : (x.maxDrivers < 2) ? 2 : x.maxDrivers;
+        if ("trackName" in x) x.trackName.toLowerCase().split(' ').map(x => x.charAt(0).toUpperCase() + x.substring(1)).join(' ');
+        if ("numberOfLaps" in x) x.numberOfLaps = (x.numberOfLaps > 100) ? 100 : (x.numberOfLaps < 1) ? 1 : x.numberOfLaps;
+        if ("upgradesAllowed" in x) x.upgradesAllowedString = x.upgradesAllowed ? "Allow upgrades" : "Stock cars only";
+        if ("betAmount" in x) x.betAmount = (x.betAmount > 10000000) ? 10000000 : (x.betAmount < 0) ? 0 : x.betAmount;
+        if ("waitTime" in x) x.waitTime = (x.waitTime > 2880) ? 2880 : (x.waitTime < 1) ? 1 : x.waitTime;
+        if ("password" in x && x.password.length > 25) x.password = x.password.substring(0, 26);
+    })
+}
+ 
+function drawPresetBar() {
+    let filterBar = $(`
+  <div class="filter-container m-top10">
+    <div class="title-gray top-round">Race Presets</div>
+ 
+    <div class="cont-gray p10 bottom-round">
+        ${presets.map((element, index) => `<button class="torn-btn preset-btn" style="margin:0 10px 10px 0">${("name" in element) ? element.name : "Preset " + (+index + 1)}</button>`)}
+    </div>
+  </div>`);
+ 
+$('#racingAdditionalContainer > .form-custom-wrap').before(filterBar);
+$('.preset-btn').each((index, element) => element.onclick = function() {fillPreset(index)});
+}
+
+  """,
+);
